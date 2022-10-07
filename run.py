92057e3889fa8ed2dd7cdc4ea4b392d04c2811e6
MandyHole <mandyhole17@gmail.com>
2022-10-07 13:32:30 +0000

    Add function getEventDate

--- run.py
+++ run.py
@@ -17,9 +17,11 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-import datetime
+from datetime import datetime
 
 def getEventType():
+    """
+    Request type of event from user (Open Day or Musician)"""
     print("Welcome to the Open Day Planner. I hope it helps to make the event run seamlessly!\n")
     event_type=input("What type of event is it? Please write Open Day or Musician: ")
     print(f"The event type you provided is {event_type}")
@@ -31,4 +33,30 @@ def getEventType():
     else:
         print("That is not a valid response. Please select Open Day or Musician, ensuring you use initial caps")
 
-getEventType()
+
+
+def getEventDate():
+    print("Please provide the date of the event")
+    print("Use the format mm/dd/yyyy \n")
+    eventDate = input("Event date: \n")
+# https://theprogrammingexpert.com/check-if-string-is-date-in-python/#:~:text=To%20check%20if%20a%20string,string%20and%20a%20date%20format.&text=When%20working%20with%20strings%20in,date%20can%20be%20very%20useful.
+    format_ddmmyyyy = "%d/%m/%Y"
+
+    try:
+        date = datetime.strptime(eventDate, format_ddmmyyyy)
+        print("The string is a date with format " + format_ddmmyyyy)
+    except ValueError:
+        print("The string is not a date with format " + format_ddmmyyyy)
+
+    
+    print(f"You provided this date {eventDate}")
+    checkDate = input("Is this correct (Y/N)")
+    if checkDate == "Y":
+        print("They typed the right date!)")
+    elif checkDate == "N":
+        print("Better try again")
+    else:
+        print("Please try again and type either 'Y' or 'N'")
+
+# getEventType()
+getEventDate()
