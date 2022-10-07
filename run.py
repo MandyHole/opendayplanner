0e162209bdef788189e00ea636bdbc09b7a96781
MandyHole <mandyhole17@gmail.com>
2022-10-07 12:06:00 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to the Open Day Planner. I hope it helps to make the event run seamlessly!")
+event_type=input("What type of event is it? Please write Open Day or Musician:  ")
+print(f"The event type you provided is {event_type}")
