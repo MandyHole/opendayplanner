1e36d26b9b6c9cc766722fd6a65acbf37652ad0e
MandyHole <mandyhole17@gmail.com>
2022-10-21 13:48:59 +0000

    Reduce long lines of code and tidy up spaces/problems

--- run.py
+++ run.py
@@ -9,9 +9,6 @@ from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from gspread_formatting import *
 from time import sleep
 
-
-
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -25,7 +22,8 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 
 stock_data = pd.DataFrame({
-    'Type of Stock': ['Highlighter', 'Luggage Tag', 'Pens', 'Pencils', 'Notebooks', 'Water Bottles'],
+    'Type of Stock': ['Highlighter', 'Luggage Tag', 'Pens', 
+                      'Pencils', 'Notebooks', 'Water Bottles'],
     'Number Remaining': ['', '', '', '', '', ''],
     'Location of Stock': ['', '', '', '', '', ''],
     'Date checked': ['', '', '', '', '', '']})
@@ -54,7 +52,8 @@ tasks_data = pd.DataFrame({
             'Remove Social Header'
             ],
     "Date Completed": ['', '', '', '', '', '', '', '', '', '', '', '', ''],
-    "Person Performing Task": ['', '', '', '', '', '', '', '', '', '', '', '', ''],
+    "Person Performing Task": ['', '', '', '', '', '', 
+                               '', '', '', '', '', '', ''],
     "Notes": ['', '', '', '', '', '', '', '', '', '', '', '', '']
 })
 
@@ -71,7 +70,9 @@ musician_tasks_data = pd.DataFrame({
     "Notes": ['', '', '', '', '']
 })
 
-print("Welcome to the Open Day Planner. I hope it helps to make the event run seamlessly!\n")
+print("Welcome to the Open Day Planner.")
+print("I hope it helps to make the event run seamlessly!\n")
+
 
 def get_event_type():
     """
@@ -79,11 +80,15 @@ def get_event_type():
     Loops until the user inputs a correct value.
     """
     while True:
+        print("Please specify the type of event that you are planning.")
+        print("This could be 'Open Day' or 'Musician'.")
+        print("Please ensure you use initial caps.")
         global event_type
-        event_type=input("What type of event are you planning? Please write Open Day or Musician: ")
+        event_type = input("Type of event: ")
         if validate_event_type(event_type):
             break
     return event_type
+
 
 def validate_event_type(values):
     """ 
@@ -92,8 +97,8 @@ def validate_event_type(values):
     """
     try:
         if values != "Open Day" and values != "Musician":
-            raise ValueError (
-                "Please input 'Open Day' or 'Musician', ensuring you use initial caps."
+            raise ValueError(
+                "Input 'Open Day' or 'Musician' using initial caps."
             )
     except ValueError as e:
         print(f"'{values}' is not a valid response. {e}\n")
@@ -108,12 +113,14 @@ def get_event_date():
     Loops until a correctly formatted date in future is input.
     """
     print("\n")
-    print("Please provide the date of the event (use the format dd/mm/yyyy) \n")
+    print("Please provide the date of the event.")
+    print("Use the format dd/mm/yyyy) \n")
     while True:
         event_date = input("Event date: \n")
         if validate_event_date(event_date):
             break
     return event_date
+
 
 def validate_event_date(date_values):
     """ 
@@ -128,13 +135,19 @@ def validate_event_date(date_values):
     # https://stackoverflow.com/questions/7239315/cant-compare-datetime-datetime-to-datetime-date
     date_today = datetime.now()
     try:
-        if date == datetime.strptime(date_values, format_ddmmyyyy) or datetime.strptime(date_values, format_ddmmyyyy) <= date_today:
-            raise ValueError ("Please ensure you use the format dd/mm/yyyy and that the date is in the future.")
+        if date == datetime.strptime(date_values, 
+                                     format_ddmmyyyy) or \
+            datetime.strptime(date_values, 
+                              format_ddmmyyyy) <= date_today:
+            raise ValueError(
+                "Use the format dd/mm/yyyy and check it is in the future.")
     except ValueError as e:
-        print(f"Please ensure you use the format dd/mm/yyyy and that the date is in the future.")
+        print("Please ensure you use the format dd/mm/yyyy.")
+        print("Please also ensure that the date is in the future.")
         print(f"{date_values}' is not a valid response. {e}\n")
         return False
     return True
+
 
 def get_date_to_check():
     """
@@ -154,19 +167,21 @@ def get_date_to_check():
     print(f"You provided this date: {date_to_check}")
     return date_to_check
 
+
 def confirm_date():
     """
     Enables user to confirm that the date provided is the correct date.
     If not, loops back to ask for the date again.
     """
-    final_date_to_check = get_date_to_check()    
+    # final_date_to_check = get_date_to_check()    
     while True:
         checkDate = input("Is this correct (Y/N)? \n")
         if check_date_validation(checkDate):
             break
     if checkDate == "N":
         confirm_date()
-    
+
+
 def check_date_validation(check_value):
     """
     Checks user input Y or N to confirm date.
@@ -174,13 +189,14 @@ def check_date_validation(check_value):
     """
     try:
         if check_value != "Y" and check_value != "N":
-            raise ValueError (
-                "Please input 'Y' for yes or 'N' for no, ensuring you use a capital letter."
+            raise ValueError(
+                "Please input 'Y' for yes or 'N' for no."
             )
     except ValueError as e:
         print(f"'{check_value}' is not a valid response. {e}\n")
         return False
     return True
+
 
 def get_email():
     """
@@ -188,16 +204,18 @@ def get_email():
     Loops until a valid email address is provided.
     Prints statements to say what email will be used for.
     """
-    print("We need your email address so you can have access to the spreadsheet and reminders")
+    print("Your email address enables access to a spreadsheet and reminders")
     while True:
         global entered_email
         entered_email = input("What is your email address?  ")
         print("\n")
         if validate_email(entered_email):
             break
-    print("Thank you for providing a valid email address so we can share the Google spreadsheet and reminders with you.\n")
-    print("Please be patient as it may take a minute for the spreadsheet to be created.\n")
+    print("Thank you for providing a valid email address.")
+    print("We can share the Google spreadsheet and reminders with you.\n")
+    print("Please be patient as the spreadsheet is created...\n")
     return entered_email
+
 
 def validate_email(s):
     """
@@ -206,11 +224,12 @@ def validate_email(s):
     """
     # https://www.tutorialspoint.com/python-program-to-validate-email-address
     pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
-    if re.match(pat,s):
+    if re.match(pat, s):
         return True
     else:
         print("That is not a valid email address; please try again.\n")
         return False
+
 
 def create_spreadsheet():
     """
@@ -221,6 +240,7 @@ def create_spreadsheet():
     spreadsheet = GSPREAD_CLIENT.create(f'{event_type}: {date_of_event}')
     spreadsheet.share(f'{entered_email}', perm_type='user', role='writer')
 
+
 def create_worksheet(sheet_name, sheet_data, final_column):
     """
     Adds a new worksheet to the spreadsheet created for the event.
@@ -228,12 +248,14 @@ def create_worksheet(sheet_name, sheet_data, final_column):
     Adds formatting to header row.
     """
     # test_spreadsheet=GSPREAD_CLIENT.open('Open Day: 14/08/2020')
-        #  change above to this once done testing to create new spreadsheet)
-    new_worksheet=spreadsheet.add_worksheet(title=sheet_name, rows=100, cols=20)
+    #  change above to this once done testing to create new spreadsheet)
+    new_worksheet = spreadsheet.add_worksheet(title=sheet_name, 
+                                              rows=100, cols=20)
     # https://medium.com/@jb.ranchana/write-and-append-dataframes-to-google-sheets-in-python-f62479460cf0
     new_worksheet.clear()
-    set_with_dataframe(worksheet=new_worksheet, dataframe=sheet_data, include_index=False,
-    include_column_header=True, resize=True)
+    set_with_dataframe(worksheet=new_worksheet, 
+                       dataframe=sheet_data, include_index=False,
+                       include_column_header=True, resize=True)
     # https://github.com/robin900/gspread-formatting
     set_column_width(new_worksheet, f'A:{final_column}', 250)
     fmt = cellFormat(
@@ -243,8 +265,6 @@ def create_worksheet(sheet_name, sheet_data, final_column):
         )
     format_cell_range(new_worksheet, f'A1:{final_column}1', fmt)
     return new_worksheet
-    
-
 # https://www.digitalocean.com/community/tutorials/update-rows-and-columns-python-pandas
 # https://docs.gspread.org/en/latest/user-guide.html
 
@@ -256,7 +276,8 @@ def calculate_reminder(x):
     Ensures event doesn't occur on a weekend.
     """
     format_ddmmyyyy = "%d/%m/%Y"
-    formatted_final_event_date = datetime.strptime(date_of_event, format_ddmmyyyy)
+    formatted_final_event_date = datetime.strptime(date_of_event, 
+                                                   format_ddmmyyyy)
     reminder = formatted_final_event_date - timedelta(days=x)
     date_today = datetime.now()
     if reminder <= date_today:
@@ -279,31 +300,32 @@ def add_event_to_calendar(description, day):
     Date of reminder is in relation to date of event.
     """
     event = {
-    'summary': f'{event_type}: {date_of_event} Tasks to Complete', 
-    'description': f'It is about {day} days until the event. Open the {event_type}: {date_of_event} spreadsheet. {description} Complete the Task Planner Spreadsheet.',
-    'start': {
-        'dateTime': calculate_reminder(day),
-        'timeZone': 'Europe/London',
-    'transparency': 'transparent',
-    },
-    'end': {
-        'dateTime': calculate_reminder(day) + timedelta(hours=1),
-        'timeZone': 'Europe/London',
-    },
-    'attendees': [
-        {'email': entered_email},
-    ],
-    'reminders': {
-        'useDefault': False,
-        'overrides': [
-        {'method': 'email', 'minutes': 0},
-        {'method': 'popup', 'minutes': 10},
+        'summary': f'{event_type}: {date_of_event} Tasks to Complete', 
+        'description': f'It is about {day} days until the event. Open the \
+            {event_type}: {date_of_event} spreadsheet. {description} \
+                Complete the Task Planner Spreadsheet.',
+        'start': {
+            'dateTime': calculate_reminder(day),
+            'timeZone': 'Europe/London',
+        },
+        'end': {
+            'dateTime': calculate_reminder(day) + timedelta(hours=1),
+            'timeZone': 'Europe/London',
+        },
+        'attendees': [
+            {'email': entered_email},
         ],
-    },
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 0},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+        'transparency': 'transparent',
+
     }
     event = service.events().insert(calendarId='primary', body=event).execute()
-
-
 
 
 def main():
@@ -317,10 +339,12 @@ def main():
         # create_worksheet('Task Planner', musician_tasks_data, 'D')
         # create_worksheet('Attendees', attendees_data, 'D')
         # create_worksheet('Stock Take', stock_data, 'D')
-    #     add_event_to_calendar('Contact Music Department and see if any boosting is required', 30)
-    #     add_event_to_calendar('Remove option from form', 7)
-    #     add_event_to_calendar('Post on social to saying looking forward to event', 1)
-    #     add_event_to_calendar('Please take a photo of the event today and post on social media', 0)
+        # add_event_to_calendar('Contact Music Department \
+        #   and see if any boosting is required', 30)
+        # add_event_to_calendar('Remove option from form', 7)
+        # add_event_to_calendar('Post on social to saying looking forward to event', 1)
+        # add_event_to_calendar('Please take a photo of the event today and post on social media', 0)
+        # https://stackoverflow.com/questions/17432478/python-print-to-one-line-with-time-delay-between-prints
         print("You will now have been shared a spreadsheet to plan the Musician Event.\n") ; sleep(3.3)
         print("You also will have reminders in your Calendar on what you need to do going forward.\n") ; sleep(3.3)
         print("Please ensure you do the following as soon as possible:\n") ; sleep(3.3)
@@ -347,6 +371,5 @@ def main():
         print("* Create a zap to link the Open Day Signup form to the Attendees worksheet.\n") ; sleep(3.6)
         print("* Ensure you initial and date these tasks are complete using the Task Planner Worksheet.\n") ; sleep(3.7)
         print("We hope this helps with your planning. Please refresh the page to plan another event.\n")
-
 
 main()
